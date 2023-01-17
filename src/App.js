e05147ea46743567bc3c02e38840b39f5b36b8e4
Joao_Godinho <git.qodeless@outlook.com>
2023-01-17 08:05:24 -0300

    Start Project

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import './Header';
+
+function App() {
+  return (
+    <div className="App">
+      <header />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Header.js
@@ -0,0 +1,9 @@
+function Header() {
+  return (
+    <header className="App-header">
+      <text>Teste</text>
+    </header>
+  );
+}
+
+export default Header;
