2b69da1d7750ad71a8696708b41a9e722d9ae4c2
Joao Eustáquio <joaoeustaquiosg@gmail.com>
2023-01-28 10:42:19 -0300

    Starting Body development with ads for purchase

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import './App.css';
+import { Layout } from 'antd';
 import Header from './components/Header.js';
+import PCGamer from './components/home/PCGamer.js';
 
 function App() {
+
+  const { Content, Footer } = Layout;
+
   return (
     <div className="App">
-      <Header/>
-      <footer className="App-footer">
+      <div>
+        <Header/>
+      </div>
 
-      </footer>
+      <Content>
+        {/* <Carousel/> */}
+        <PCGamer/>
+      </Content>
+
+      <Footer className="App-footer"/>
     </div>
   );
 }
---
+++ src/components/home/PCGamer.js
@@ -0,0 +1,17 @@
+import '../css/PCGamer.css';
+import Box from '@mui/material/Box';
+import "@fontsource/lexend-deca";
+// import Button from '@mui/material/Button';
+// import ButtonGroup from '@mui/material/ButtonGroup';
+
+export default function PCGamer() {
+    // const openInNewTab = (url) => {
+    //     window.open(url, '_blank', 'noreferrer');
+    // };
+
+    return (
+        <Box className="App-PCGamer">
+            <text style={{color: "red"}}>TESTE</text>
+        </Box>
+    );
+}
