9544296e855385b5f8d2f48a08f99a8c621499a1
Joao Eustáquio <joaoeustaquiosg@gmail.com>
2023-01-30 16:36:48 -0300

    Clearing console warnings

--- src/components/home/Carousel.js
+++ src/components/home/Carousel.js
@@ -1,36 +1,57 @@
 import "../css/Carousel.css";
 import "@fontsource/lexend-deca";
 import { Carousel } from "antd";
-import Box from '@mui/material/Box';
+import Box from "@mui/material/Box";
 import styled from "styled-components";
+
+const H3 = styled.h3`
+height: "160px";
+color: "#fff";
+line-height: "160px";
+text-align: "center";
+background: "#364d79";
+`;
 
 export default function carousel() {
   // const openInNewTab = (url) => {
   //     window.open(url, '_blank', 'noreferrer');
   // };
 
-  const H3 = styled.h3 `
-    height: '160px';
-    color: '#fff';
-    line-height: '160px';
-    text-align: 'center';
-    background: '#364d79';
-  `
-
-  return (    
-    <Carousel autoplay>
-      <div>
-        <H3><Box><img role="link" className="Banner" src='./BannerTest.jpg' alt='Banner1' /></Box></H3>
-      </div>
-      <div>
-        <H3><Box><img role="link" src='./BannerTest.jpg' alt='Banner1' /></Box></H3>
-      </div>
-      <div>
-        <H3><Box><img role="link" src='./BannerTest.jpg' alt='Banner1' /></Box></H3>
-      </div>
-      <div>
-        <H3><Box><img role="link" src='./BannerTest.jpg' alt='Banner1' /></Box></H3>
-      </div>
-    </Carousel>
+  return (
+      <Carousel autoplay>
+        <div>
+          <H3>
+            <Box>
+              <img
+                role="link"
+                className="Banner"
+                src="./BannerTest.jpg"
+                alt="Banner1"
+              />
+            </Box>
+          </H3>
+        </div>
+        <div>
+          <H3>
+            <Box>
+              <img role="link" src="./BannerTest.jpg" alt="Banner1" />
+            </Box>
+          </H3>
+        </div>
+        <div>
+          <H3>
+            <Box>
+              <img role="link" src="./BannerTest.jpg" alt="Banner1" />
+            </Box>
+          </H3>
+        </div>
+        <div>
+          <H3>
+            <Box>
+              <img role="link" src="./BannerTest.jpg" alt="Banner1" />
+            </Box>
+          </H3>
+        </div>
+      </Carousel>
   );
 }
