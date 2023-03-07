b4d0efb5b700b2c4830160bea394f0f1a79cf115
Joao_Godinho <git.qodeless@outlook.com>
2023-03-07 00:07:10 -0300

    Start Catalog

--- src/components/home/Catalog.js
+++ src/components/home/Catalog.js
@@ -9,23 +9,33 @@ import "../css/Catalog.css";
 
 export default function MediaCard() {
   return (
-    <Card sx={{ maxWidth: 345 }}>
+    <Card sx={{ maxWidth: 365 }}>
       <CardMedia
         component="img"
-        height="300"
         image="https://m.media-amazon.com/images/I/613yvYP-TkL._AC_SY450_.jpg"
       />
-      <CardContent>
+      <CardContent className="catalogCard" >
         <Typography className="title" gutterBottom variant="h5" component="div">
           Kit Pc Gamer, teclado, mouse e monitor
         </Typography>
         <Typography className="config" variant="body2" color="text.secondary">
           CONFIGURAÇÕES
         </Typography>
+
+        <img sx={{ maxWidth: 36 }} className="TN4Logo" src='./pix-icon.png' alt='TN4Logo' />
+        <Typography className="title" gutterBottom variant="h5" component="div">
+          R$4590,00 no PIX
+        </Typography>
+
+
+        <Typography className="title" gutterBottom variant="h5" component="div">
+          R$4890,00 em até 10x no cartão SEM JUROS
+        </Typography>
+
+        <CardActions className="buyBTN">
+          <Button size="large" className="buyBTN2" >COMPRAR AGORA</Button>
+        </CardActions>
       </CardContent>
-      <CardActions>
-        <Button size="large" className="buyBTN">COMPRAR AGORA</Button>
-      </CardActions>
     </Card>
   );
 }
