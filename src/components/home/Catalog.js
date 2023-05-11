337bebc2bfcf0147b89c3953b6f09e8336842924
Joao_Godinho <git.qodeless@outlook.com>
2023-05-11 07:55:26 -0300

    [CATALOG] Adding some changes to the catalog

--- src/components/home/Catalog.js
+++ src/components/home/Catalog.js
@@ -10,32 +10,37 @@ export default function MediaCard() {
   return (
     <>
       <div className="Cards">
-        <Card sx={{ maxWidth: 250, maxHeight: 600 }}>
+        <Card sx={{ maxWidth: 260, maxHeight: 600 }}>
           <CardMedia
             component="img"
             image="https://shopinfo.vteximg.com.br/arquivos/ids/1351540-1000-1000/1.png?v=638143379900800000"
           />
+
           <CardContent className="catalogCard">
             <Typography className="title" component="div">
               Kit Pc Gamer, teclado, mouse e monitor
             </Typography>
-            <Typography
-              className="config"
+            <Typography className="config">CONFIGURAÇÕES</Typography>
+
+            <div className="PixPay">
+              <img className="PixLogo" src="./pix-icon.png" alt="PixLogo" />
+              <Typography className="PixValue" component="div">
+                R$4590,00 no PIX
+              </Typography>
+            </div>
+
+            <div className="CardPay">
+              <img className="CardLogo" src="./Card-Icon.png" alt="CardLogo" />
+              <Typography className="CardValue" component="div">
+                R$4890,00 em até 10x SEM JUROS
+              </Typography>
+            </div>
+
+            <Button
+              classes={{ width: 400 }}
+              variant="contained"
+              className="buyBTN"
             >
-              CONFIGURAÇÕES
-            </Typography>
-
-            <img className="PixLogo" src="./pix-icon.png" alt="PixLogo" />
-            <Typography className="valueOne" component="div">
-              R$4590,00 no PIX
-            </Typography>
-
-            <img className="CardLogo" src="./Card-Icon.png" alt="CardLogo" />
-            <Typography className="valueTwo" component="div">
-              R$4890,00 em até 10x SEM JUROS
-            </Typography>
-
-            <Button classes={{ width:400 }} variant="contained" className="buyBTN">
               COMPRAR AGORA
             </Button>
           </CardContent>
